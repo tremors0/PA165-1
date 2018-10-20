1de5b57497fcda21193ed25d9ecda1760dfa5ee9
adamkral12 <kral.adam95@gmail.com>
2018-10-20 11:50:58 +0200

    Ak/feature/agent entity (#3)
    * add agent entity
    
    * add constructor doc
    
    * add submodule to base module
    
    * set pom packaging

---
+++ Secret-agency-persistence/src/main/cz/fi/muni/pa165/secretagency/entity/Agent.java
@@ -0,0 +1,98 @@
+package main.cz.fi.muni.pa165.secretagency.entity;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+/**
+ * Agent entity for the Secret agency project.
+ *
+ * @author (name = "Adam Kral", UCO = "<433328>")
+ */
+
+public class Agent {
+    private Long id;
+
+    private String name;
+
+    private LocalDateTime birthDate;
+
+    /**
+     *
+     * @param id of agent
+     * @param name of agent
+     * @param birthDate of agent
+     */
+    public Agent(Long id, String name, LocalDateTime birthDate) {
+        this.id = id;
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+
+
+    /**
+     * @param id of agent
+     */
+    public Agent(Long id) {
+        this.id = id;
+    }
+
+    /** Create empty Agent **/
+    public Agent() {}
+
+    /**
+     * @return Agent's id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id agent's id
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return agent'a name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name agent's name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return agent's birth date
+     */
+    public LocalDateTime getBirthDate() {
+        return birthDate;
+    }
+
+    /**
+     * @param birthDate agent's birth date
+     */
+    public void setBirthDate(LocalDateTime birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Agent)) return false;
+        Agent agent = (Agent) o;
+        return Objects.equals(getId(), agent.getId()) &&
+                Objects.equals(getName(), agent.getName()) &&
+                Objects.equals(getBirthDate(), agent.getBirthDate());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getBirthDate());
+    }
+}
