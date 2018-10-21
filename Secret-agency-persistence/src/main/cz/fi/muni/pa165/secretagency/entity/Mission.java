f8948569dadb8b5285a59222d8af6b3d940951a7
Adam <>
2018-10-21 16:19:19 +0200

    mission-entity Added "Mission" entity class

---
+++ Secret-agency-persistence/src/main/cz/fi/muni/pa165/secretagency/entity/Mission.java
@@ -0,0 +1,129 @@
+package main.cz.fi.muni.pa165.secretagency.entity;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+/**
+ * Mission entity for Secret agency project.
+ *
+ * @author Adam Skurla (487588)
+ */
+public class Mission {
+    private Long id;
+    private Double[] location;
+    private String missionType;
+    private LocalDate started;
+    private LocalDate ended;
+
+    /**
+     * Constructor
+     * @param id of mission
+     * @param location of mission (latitude and longtitude)
+     * @param missionType type of mission
+     * @param started mission start date
+     * @param ended mission end date
+     */
+    public Mission(Long id, Double[] location, String missionType, LocalDate started, LocalDate ended) {
+        this.id = id;
+        this.location = location;
+        this.missionType = missionType;
+        this.started = started;
+        this.ended = ended;
+    }
+
+    /**
+     * Create empty Mission
+     */
+    public Mission() {
+    }
+
+    /**
+     * @return mission id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id mission id
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return mission location
+     */
+    public Double[] getLocation() {
+        return location;
+    }
+
+    /**
+     * @param location of mission
+     */
+    public void setLocation(Double[] location) {
+        this.location = location;
+    }
+
+    /**
+     * @return mission type
+     */
+    public String getMissionType() {
+        return missionType;
+    }
+
+    /**
+     * @param missionType type of mission
+     */
+    public void setMissionType(String missionType) {
+        this.missionType = missionType;
+    }
+
+    /**
+     * @return mission start date
+     */
+    public LocalDate getStarted() {
+        return started;
+    }
+
+    /**
+     * @param started start date of mission
+     */
+    public void setStarted(LocalDate started) {
+        this.started = started;
+    }
+
+    /**
+     * @return mission end date
+     */
+    public LocalDate getEnded() {
+        return ended;
+    }
+
+    /**
+     * @param ended end date of mission
+     */
+    public void setEnded(LocalDate ended) {
+        this.ended = ended;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        if (!super.equals(object)) return false;
+        Mission mission = (Mission) object;
+        return java.util.Objects.equals(id, mission.id) &&
+                java.util.Arrays.equals(location, mission.location) &&
+                java.util.Objects.equals(missionType, mission.missionType) &&
+                java.util.Objects.equals(started, mission.started) &&
+                java.util.Objects.equals(ended, mission.ended);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(super.hashCode(), id, missionType, started, ended);
+        result = 31 * result + Arrays.hashCode(location);
+        return result;
+    }
+}
