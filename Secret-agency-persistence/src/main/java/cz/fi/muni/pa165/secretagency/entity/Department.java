e363f8666266a800b1328d77033ba467894f6bde
Miloš Šilhár <xsilhar@fi.muni.cz>
2018-10-25 23:55:25 +0200

    Department fix

--- Secret-agency-persistence/src/main/java/cz/fi/muni/pa165/secretagency/entity/Department.java
+++ Secret-agency-persistence/src/main/java/cz/fi/muni/pa165/secretagency/entity/Department.java
@@ -7,7 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.Collections;
