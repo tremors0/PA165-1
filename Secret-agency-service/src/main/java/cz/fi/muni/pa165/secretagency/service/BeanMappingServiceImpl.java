f5683961c6d51017e4447c1eb0f441efb7e86e51
Jan Pavlů <janpavlu1@gmail.com>
2018-11-24 09:05:17 +0100

    Service for mapping Entities to DTOs taken from example project. (#58)

---
+++ Secret-agency-service/src/main/java/cz/fi/muni/pa165/secretagency/service/BeanMappingServiceImpl.java
@@ -0,0 +1,36 @@
+package cz.fi.muni.pa165.secretagency.service;
+
+import org.dozer.Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Service implementation for mapping between entities and DTOs taken from example project.
+ */
+@Service
+public class BeanMappingServiceImpl implements BeanMappingService {
+
+    @Autowired
+    private Mapper dozer;
+
+    public  <T> List<T> mapTo(Collection<?> objects, Class<T> mapToClass) {
+        List<T> mappedCollection = new ArrayList<>();
+        for (Object object : objects) {
+            mappedCollection.add(dozer.map(object, mapToClass));
+        }
+        return mappedCollection;
+    }
+
+    public  <T> T mapTo(Object u, Class<T> mapToClass)
+    {
+        return dozer.map(u,mapToClass);
+    }
+
+    public Mapper getMapper(){
+        return dozer;
+    }
+}
