11b8955b3b06610a829cfbb9200e8eb370e09534
tremors0 <janpavlu1@gmail.com>
2018-11-25 22:05:53 +0100

    Add DirtiesContext annotation.

--- Secret-agency-service/src/test/java/cz/fi/muni/pa165/secretagency/service/facade/ReportFacadeUnitTest.java
+++ Secret-agency-service/src/test/java/cz/fi/muni/pa165/secretagency/service/facade/ReportFacadeUnitTest.java
@@ -16,6 +16,7 @@ import cz.fi.muni.pa165.secretagency.service.config.ServiceConfiguration;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.springframework.test.util.ReflectionTestUtils;
@@ -36,6 +37,7 @@ import static org.mockito.Mockito.*;
  * @author Jan Pavlu
  */
 @ContextConfiguration(classes = ServiceConfiguration.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public class ReportFacadeUnitTest extends AbstractTestNGSpringContextTests {
 
     @Mock
