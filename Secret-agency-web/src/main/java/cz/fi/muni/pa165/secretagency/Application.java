b4f7b927e80b69d5460d0053589066bffd21084e
tremors0 <janpavlu1@gmail.com>
2018-12-09 20:14:39 +0100

    Basic configuration for web.

---
+++ Secret-agency-web/src/main/java/cz/fi/muni/pa165/secretagency/Application.java
@@ -0,0 +1,13 @@
+package cz.fi.muni.pa165.secretagency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+
+}
