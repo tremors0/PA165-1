39aff40ca4d0a2e43c8d3a9ec6d9d4feea922d81
tremors0 <janpavlu1@gmail.com>
2018-12-13 21:05:54 +0100

    incomplete agent type

---
+++ Secret-agency-web/src/main/react/secret-agency/src/types/Agent.d.ts
@@ -0,0 +1,12 @@
+export interface IAgent {
+  readonly id: number;
+  readonly name: string;
+  readonly birthDate: Date;
+  readonly languages: string[];
+  // private AgentRankEnum rank;
+  readonly codeName: string;
+  readonly passwordHash: string;
+  // private DepartmentDTO department;
+  readonly missionIds: number[];
+  readonly reportIds: number[];
+}
