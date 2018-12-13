f4b02ad2178094c5ca6b806476bfa853e503811a
tremors0 <janpavlu1@gmail.com>
2018-12-13 20:59:32 +0100

    Spring security configuration.

--- Secret-agency-web/src/main/java/cz/fi/muni/pa165/secretagency/Application.java
+++ Secret-agency-web/src/main/java/cz/fi/muni/pa165/secretagency/Application.java
@@ -1,12 +1,13 @@
 package cz.fi.muni.pa165.secretagency;
 
+import cz.fi.muni.pa165.secretagency.security.CorsConfigurationImpl;
 import cz.muni.fi.pa165.secretagency.InitDataConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import(InitDataConfiguration.class)
+@Import({InitDataConfiguration.class, CorsConfigurationImpl.class})
 public class Application {
 
     public static void main(String[] args) {
---
+++ Secret-agency-web/src/main/java/cz/fi/muni/pa165/secretagency/security/AuthenticationProviderImpl.java
@@ -0,0 +1,58 @@
+package cz.fi.muni.pa165.secretagency.security;
+
+import cz.fi.muni.pa165.secretagency.dto.AgentAuthenticateDTO;
+import cz.fi.muni.pa165.secretagency.dto.AgentDTO;
+import cz.fi.muni.pa165.secretagency.enums.AgentRankEnum;
+import cz.fi.muni.pa165.secretagency.facade.AgentFacade;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class AuthenticationProviderImpl implements AuthenticationProvider {
+
+    @Autowired
+    private AgentFacade agentFacade;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String codeName = authentication.getName();
+        String password = (String) authentication.getCredentials();
+
+        // TODO - create custom exception
+        AgentDTO agentDTO;
+        try {
+            agentDTO = agentFacade.getAgentByCodeName(codeName);
+        } catch (NullPointerException e) {
+            throw new BadCredentialsException(e.getMessage());
+        }
+
+        AgentAuthenticateDTO authenticateDTO = new AgentAuthenticateDTO();
+        authenticateDTO.setUserId(agentDTO.getId());
+        authenticateDTO.setPassword(password);
+
+
+        // TODO - throw custom exception when result is false
+        if (!agentFacade.authenticate(authenticateDTO)) {
+            throw new BadCredentialsException("Invalid password or e-mail");
+        }
+
+        String userRole = agentDTO.getRank() == AgentRankEnum.AGENT_IN_CHARGE ? "ADMIN" : "USER";
+
+        List<GrantedAuthority> authorities = AuthorityUtils.createAuthorityList(userRole);
+        return new UsernamePasswordAuthenticationToken(codeName, password, authorities);
+    }
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return aClass.equals(UsernamePasswordAuthenticationToken.class);
+    }
+}
