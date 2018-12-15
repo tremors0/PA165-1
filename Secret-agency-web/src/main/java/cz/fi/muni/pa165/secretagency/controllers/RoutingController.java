2d017552499d84c95fde12e74faabddc24f38bbb
tremors0 <janpavlu1@gmail.com>
2018-12-15 20:01:05 +0100

    Added javadoc

--- Secret-agency-web/src/main/java/cz/fi/muni/pa165/secretagency/controllers/RoutingController.java
+++ Secret-agency-web/src/main/java/cz/fi/muni/pa165/secretagency/controllers/RoutingController.java
@@ -4,6 +4,13 @@ import cz.fi.muni.pa165.secretagency.ApiUris;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * Controller which supports React routing. To make React routing work when front-end is deployed to the same server
+ *   as back-end, it's necessary to provide endpoints which always return index.html.<br>
+ * For this app, every endpoint which path is starting with <b></>pa165/secretAgency/</b> returns index.html.
+ *
+ * @author Jan Pavlu
+ */
 @Controller
 public class RoutingController {
 
