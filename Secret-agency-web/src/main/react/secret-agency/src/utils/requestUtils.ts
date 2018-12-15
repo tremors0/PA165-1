935ab2a05595bdf75a48db52ae6a243e0c1c1f76
tremors0 <janpavlu1@gmail.com>
2018-12-15 15:43:17 +0100

    If user was logged out, local storage data are removed. Check every request.

--- Secret-agency-web/src/main/react/secret-agency/src/utils/requestUtils.ts
+++ Secret-agency-web/src/main/react/secret-agency/src/utils/requestUtils.ts
@@ -1,4 +1,5 @@
-import axios, {AxiosPromise} from "axios";
+import axios, {AxiosPromise, AxiosResponse} from "axios";
+import * as secretAgencyRepository from "../repository/secretAgecyRepository";
 
 // absolute path is used to be able send requests both from node server and tomcat
 export const URL_BASE = "http://localhost:8080/pa165";
@@ -6,17 +7,48 @@ export const REST_URL_BASE = "http://localhost:8080/pa165/rest";
 
 // helper request methods - for authorized request
 export function GET<T>(url: string): AxiosPromise<T> {
-  return axios.get<T>(url, {withCredentials: true});
+    return axios.get<T>(url, {withCredentials: true}).then((response) => {
+        validateResponse(response);
+        return response;
+    });
 }
 
 export function POST<T>(url: string, data?: any): AxiosPromise<T> {
-  return axios.post<T>(url, data, {withCredentials: true});
+    return axios.post<T>(url, data, {withCredentials: true}).then((response) => {
+        validateResponse(response);
+        return response;
+    });
 }
 
 export function PUT<T>(url: string, data: any): AxiosPromise<T> {
-  return axios.put<T>(url, data, {withCredentials: true});
+    return axios.put<T>(url, data, {withCredentials: true}).then((response) => {
+        validateResponse(response);
+        return response;
+    });
 }
 
 export function DELETE<T>(url: string): AxiosPromise<T> {
-  return axios.delete(url, {withCredentials: true});
+    return axios.delete(url, {withCredentials: true}).then((response) => {
+        validateResponse(response);
+        return response;
+    });
+}
+
+/**
+ * Check if user was logged out. If that's true, it removes authenticated user data from local storage and reloads page.
+ * @param response server response for last request
+ */
+function validateResponse(response: AxiosResponse): void {
+    // if response is redirection or HTML document, user was logged out
+    if (response.status === 301 || response.status === 302 || response.status === 303 ||
+        isResponseHtml(response.data)) {
+        const agentRemoved = secretAgencyRepository.removeAuthenticatedAgent();
+        if (agentRemoved) {
+            location.reload();
+        }
+    }
+}
+
+function isResponseHtml(responseData: any): boolean {
+    return typeof responseData === "string" && responseData.indexOf("<!DOCTYPE HTML>") !== -1;
 }
